3b7249eb1364151946ad3c6c39b9f3d1862cfa5e
necro <necroforger@gmail.com>
2018-08-02 09:00:32 -0400

    scrape

---
+++ scrape/scrape.go
@@ -0,0 +1,17 @@
+/*
+Package scrape provides some methods for scraping information from youtube
+
+- TODO
+[ ] Channel subscriptions
+[ ] Channel playlists
+*/
+package scrape
+
+// Subscription ...
+type Subscription struct {
+}
+
+// Subscriptions ...
+func Subscriptions(URL string) Subscription {
+	return Subscription{}
+}
