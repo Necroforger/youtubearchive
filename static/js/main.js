e5ea00c96169cadaec2ae7381d222a384b985a0a
necro <necroforger@gmail.com>
2018-08-09 08:12:00 -0400

    Set expiration date on CSS cookies

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
 /**
- * @description Sets the CSS cookie and reloads the window
- * 				for it to take effect
+ * @description Sets the CSS cookie and reloads the window for it to take effect
  * @param {string} name the string to set the css cookie to
  */
 function setCSS(name) {
-	document.cookie = `css=${name}; path=/; expires=09/09/9999`;
+	let d = new Date();
+	document.cookie = `css=${name}; path=/; expires=${new Date(d.getFullYear() + 9, d.getMonth(), d.getDay()).toUTCString()}`;
 	window.location.reload();
 }
 
