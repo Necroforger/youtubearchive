ee886c4ddd0f315df86d4512880ffdb29d45e1bb
necro <necroforger@gmail.com>
2019-06-04 17:31:28 -0400

    track channel scraper files

---
+++ cmd/ytatool/channel_scraper.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+TODO: Classes and other scraping information below
+
+.channel-header-profile-image
+.about-description
+.about-stats
+.yt-lockup-playlist
+
+You can also find playlists by using a CSS selector looking for links that look like
+/playlist?list=PLNCRTSKrIMvss_8PSICTJJxUWKUgSu2nU
+*/
---
+++ cmd/ytatool/updateChannelMetadata.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Necroforger/youtubearchive"
+
+	"github.com/jinzhu/gorm"
+)
+
+// Channel holds channel information
+type Channel struct {
+	Name string
+	URL  string
+}
+
+func updateChannelMetadataCmd(db *gorm.DB) {
+	var channels []Channel
+	rows, err := db.Raw("SELECT uploader, uploader_url FROM terminated_channels WHERE not terminated").Rows()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Scam all results into array of channels
+	for rows.Next() {
+		var t = Channel{}
+		err = rows.Scan(&t.Name, &t.URL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		channels = append(channels, t)
+	}
+
+	// Archive each channel's metadata
+	for _, v := range channels {
+		fmt.Println("archiving: ", v.Name)
+		youtubearchive.ArchiveChannelMetadata(db, v.URL)
+	}
+}
